b7c72904b7f548a9240dfdb15343e8816484b4e6
jrfertonani <ademirjuniorfertonani@gmail.com>
2023-12-28 23:10:05 -0300

    start

---
+++ helpdesck/src/test/java/com/junior/helpdesck/HelpdesckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junior.helpdesck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdesckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
