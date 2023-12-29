d78d1afae06f58ed6c10bef33aff3265290c16d4
jrfertonani <ademirjuniorfertonani@gmail.com>
2023-12-29 00:22:42 -0300

    anotacoes jpa para classes domain

--- helpdesck/src/main/java/com/junior/helpdesck/domain/Cliente.java
+++ helpdesck/src/main/java/com/junior/helpdesck/domain/Cliente.java
@@ -1,18 +1,28 @@
 package com.junior.helpdesck.domain;
 
+import com.junior.helpdesck.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cliente extends Pessoa{
+@Entity
+public class Cliente extends Pessoa implements Serializable {
+    private static final long serialVersionUID = 1L;
 
+    @OneToMany(mappedBy = "cliente")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Cliente() {
         super();
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Cliente(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.CLIENTE);
     }
 
     public List<Chamado> getChamados() {
--- helpdesck/src/main/java/com/junior/helpdesck/domain/Tecnico.java
+++ helpdesck/src/main/java/com/junior/helpdesck/domain/Tecnico.java
@@ -1,18 +1,28 @@
 package com.junior.helpdesck.domain;
 
+import com.junior.helpdesck.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Tecnico extends Pessoa{
+@Entity
+public class Tecnico extends Pessoa implements Serializable {
+    private static final long serialVersionUID = 1L;
 
+    @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Tecnico() {
         super();
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.CLIENTE);
     }
 
     public List<Chamado> getChamados() {
