54fe6ba14d171ac6c31205eada7d2562ed67ac81
jrfertonani <ademirjuniorfertonani@gmail.com>
2023-12-30 22:02:17 -0300

    endpoin findAll para Tecnico

--- helpdesck/src/main/java/com/junior/helpdesck/services/TecnicoService.java
+++ helpdesck/src/main/java/com/junior/helpdesck/services/TecnicoService.java
@@ -6,6 +6,7 @@ import com.junior.helpdesck.services.exceptions.ObjectnotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,4 +21,7 @@ public class TecnicoService {
 
     }
 
+    public List<Tecnico> findAll() {
+        return repository.findAll();
+    }
 }
